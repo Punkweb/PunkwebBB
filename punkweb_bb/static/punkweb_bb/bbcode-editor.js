c85a23cf9c3069c6125dfbcb85f438943de11fa6
jprice <jprice@izeni.com>
2023-08-20 20:38:33 -0600

    Disable emoticons from sceditor

--- punkweb_bb/static/punkweb_bb/bbcode-editor.js
+++ punkweb_bb/static/punkweb_bb/bbcode-editor.js
@@ -1,6 +1,11 @@
 $(function () {
   $(document).ready(function () {
     $(".bbcode-editor").sceditor({
+      emoticonsCompat: true,
+      emoticonsEnabled: false,
+      emoticonsRoot: "/media/precise_bbcode/smilies/",
+      fonts:
+        "Arial,Arial Black,Comic Sans MS,Inconsolata,Courier New,Georgia,Impact,Open Sans,Montserrat,Sans-serif,Serif,Times New Roman,Trebuchet MS,Verdana",
       format: "bbcode",
       icons: "material",
       style: "/static/punkweb_bb/bbcode-editor-content.css",
