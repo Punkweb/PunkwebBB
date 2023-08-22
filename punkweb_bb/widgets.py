63a9fdc6babe7427c302b4e917e89fefbedfa545
jprice <jprice@izeni.com>
2023-08-22 08:19:07 -0600

    Move jquery, sceditor, open-color and prism to /static/punkweb_bb/vendor/

--- punkweb_bb/widgets.py
+++ punkweb_bb/widgets.py
@@ -9,13 +9,13 @@ class BBCodeEditor(forms.Textarea):
     class Media:
         css = {
             "all": (
-                "/static/punkweb_bb/sceditor-3.2.0/minified/themes/square.min.css",
+                "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/themes/square.min.css",
                 "/static/punkweb_bb/bbcode-editor.css",
             )
         }
         js = (
-            "/static/punkweb_bb/jquery-3.7.0.min.js",
-            "/static/punkweb_bb/sceditor-3.2.0/minified/jquery.sceditor.bbcode.min.js",
-            "/static/punkweb_bb/sceditor-3.2.0/minified/icons/material.js",
+            "/static/punkweb_bb/vendor/jquery-3.7.0.min.js",
+            "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/jquery.sceditor.bbcode.min.js",
+            "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/icons/material.js",
             "/static/punkweb_bb/bbcode-editor.js",
         )
