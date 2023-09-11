0a78b12edf0a7b2a7a531b0cf8c908d9854f5c22
jprice <jprice@izeni.com>
2023-09-11 09:31:19 -0600

    Register ProfileOnlineCacheMiddleware in example_project settings

--- example_project/settings.py
+++ example_project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "punkweb_bb.middleware.ProfileOnlineCacheMiddleware",
 ]
 
 ROOT_URLCONF = "example_project.urls"
