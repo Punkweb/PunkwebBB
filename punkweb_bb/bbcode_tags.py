3f34def2e0c573b75f95c1986117b0515e55dd99
jprice <jprice@izeni.com>
2024-04-06 01:25:48 -0600

    Add the following custom bbcode tags: CheckedBoxed, Escape, Left, Quote, Right, UncheckedBox

--- punkweb_bb/bbcode_tags.py
+++ punkweb_bb/bbcode_tags.py
@@ -2,6 +2,18 @@ from precise_bbcode.bbcode.tag import BBCodeTag
 from precise_bbcode.tag_pool import tag_pool
 
 
+class CheckedBoxBBCodeTag(BBCodeTag):
+    name = "y"
+    definition_string = "[y]{TEXT}"
+    format_string = """<label style="display: block"><input type="checkbox" disabled="disabled" checked="checked" /> {TEXT}</label>"""
+
+    class Options:
+        newline_closes = True
+        same_tag_closes = True
+        end_tag_closes = True
+        strip = True
+
+
 class CodeBBCodeTag(BBCodeTag):
     name = "code"
 
@@ -22,6 +34,15 @@ class EmailBBCodeTag(BBCodeTag):
     format_string = """<a href="mailto:{TEXT1}">{TEXT2}</a>"""
 
 
+class EscapeBBCodeTag(BBCodeTag):
+    name = "escape"
+    definition_string = "[escape]{TEXT}[/escape]"
+    format_string = """<div class="escaped">{TEXT}</div>"""
+
+    class Options:
+        render_embedded = False
+
+
 class FontBBCodeTag(BBCodeTag):
     name = "font"
     definition_string = "[font={TEXT1}]{TEXT2}[/font]"
@@ -37,16 +58,47 @@ class HrBBCodeTag(BBCodeTag):
         standalone = True
 
 
+class LeftBBCodeTag(BBCodeTag):
+    name = "left"
+    definition_string = "[left]{TEXT}[/left]"
+    format_string = """<div style="text-align: left">{TEXT}</div>"""
+
+
 class LiBBCodeTag(BBCodeTag):
     name = "li"
     definition_string = "[li]{TEXT}[/li]"
     format_string = "<li>{TEXT}</li>"
+
+    class Options:
+        strip = True
 
 
 class OlBBCodeTag(BBCodeTag):
     name = "ol"
     definition_string = "[ol]{TEXT}[/ol]"
     format_string = "<ol>{TEXT}</ol>"
+
+    class Options:
+        transform_newlines = False
+        strip = True
+
+
+class QuoteBBCodeTag(BBCodeTag):
+    name = "quote"
+    definition_string = "[quote={TEXT1}]{TEXT2}[/quote]"
+    format_string = """
+        <blockquote>
+            <cite>
+                {TEXT1} said:
+            </cite>
+            {TEXT2}
+        </blockquote>"""
+
+
+class RightBBCodeTag(BBCodeTag):
+    name = "right"
+    definition_string = "[right]{TEXT}[/right]"
+    format_string = """<div style="text-align: right">{TEXT}</div>"""
 
 
 class ShadowBBCodeTag(BBCodeTag):
@@ -78,15 +130,38 @@ class UlBBCodeTag(BBCodeTag):
     definition_string = "[ul]{TEXT}[/ul]"
     format_string = "<ul>{TEXT}</ul>"
 
+    class Options:
+        transform_newlines = False
+        strip = True
 
+
+class UncheckedBoxBBCodeTag(BBCodeTag):
+    name = "n"
+    definition_string = "[n]{TEXT}"
+    format_string = """
+        <label style="display: block"><input type="checkbox" disabled="disabled" /> {TEXT}</label>"""
+
+    class Options:
+        newline_closes = True
+        same_tag_closes = True
+        end_tag_closes = True
+        strip = True
+
+
+tag_pool.register_tag(CheckedBoxBBCodeTag)
 tag_pool.register_tag(CodeBBCodeTag)
 tag_pool.register_tag(EmailBBCodeTag)
+tag_pool.register_tag(EscapeBBCodeTag)
 tag_pool.register_tag(FontBBCodeTag)
 tag_pool.register_tag(HrBBCodeTag)
+tag_pool.register_tag(LeftBBCodeTag)
 tag_pool.register_tag(LiBBCodeTag)
 tag_pool.register_tag(OlBBCodeTag)
+tag_pool.register_tag(QuoteBBCodeTag)
+tag_pool.register_tag(RightBBCodeTag)
 tag_pool.register_tag(ShadowBBCodeTag)
 tag_pool.register_tag(SizeBBCodeTag)
 tag_pool.register_tag(SubscriptBBCodeTag)
 tag_pool.register_tag(SuperscriptBBCodeTag)
 tag_pool.register_tag(UlBBCodeTag)
+tag_pool.register_tag(UncheckedBoxBBCodeTag)
