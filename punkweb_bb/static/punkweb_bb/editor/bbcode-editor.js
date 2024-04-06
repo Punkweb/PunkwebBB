1942b36025b167977741f5d0c7f08af49030c100
jprice <jprice@izeni.com>
2024-04-06 01:36:07 -0600

    Match sceditor content to what is rendered better

--- punkweb_bb/static/punkweb_bb/editor/bbcode-editor.js
+++ punkweb_bb/static/punkweb_bb/editor/bbcode-editor.js
@@ -5,16 +5,13 @@ $(function () {
       emoticonsEnabled: true,
       emoticonsRoot: "/media/precise_bbcode/smilies/",
       emoticons: {
-        dropdown: {
-          ":smile:": "smile.png",
-          ":cool:": "cool.png",
-        },
+        dropdown: {},
       },
       format: "bbcode",
       icons: "material",
       style: "/static/punkweb_bb/editor/bbcode-editor-content.css",
       toolbar:
-        "removeformat,bold,italic,size,color,font,strike,underline|orderedlist,bulletlist|left,center,right|link,image,emoticon|horizontalrule,quote,code|maximize,source",
+        "removeformat,bold,italic,size,color,font,strike,underline|orderedlist,bulletlist|left,center,right|link,image|horizontalrule,quote,code|maximize,source",
       height: "300px",
       width: "100%",
     });
