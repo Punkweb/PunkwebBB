128caa0e460e17f8ab50656c3ecd45343e4ccfaa
jprice <jprice@izeni.com>
2024-05-29 15:16:46 -0600

    Remove Group Badges
    - Show highest priority group underneath username instead of group badges

--- punkweb_bb/templatetags/styled_group_name.py
+++ punkweb_bb/templatetags/styled_group_name.py
@@ -1,11 +1,11 @@
 from django import template
 from django.utils.safestring import mark_safe
 
-from punkweb_bb.utils import get_group_name_styled
+from punkweb_bb.utils import get_styled_group_name
 
 register = template.Library()
 
 
 @register.filter
 def styled_group_name(group):
-    return mark_safe(get_group_name_styled(group))
+    return mark_safe(get_styled_group_name(group))
