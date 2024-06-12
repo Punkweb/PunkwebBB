e467a33da3312f0a8cd2f4eac9e273a00022f686
jprice <jprice@izeni.com>
2024-06-11 23:29:42 -0600

    Reorganize static files a bit

--- punkweb_bb/static/punkweb_bb/js/bbcode-editor.js
+++ punkweb_bb/static/punkweb_bb/js/bbcode-editor.js
@@ -4,7 +4,7 @@ $(function () {
       emoticonsEnabled: false,
       format: "bbcode",
       icons: "material",
-      style: "/static/punkweb_bb/editor/bbcode-editor-content.css",
+      style: "/static/punkweb_bb/css/bbcode-editor-content.css",
       toolbar:
         "removeformat,bold,italic,size,color,font,strike,underline|orderedlist,bulletlist|left,center,right|link,image|horizontalrule,quote,code|maximize,source",
       height: "300px",
--- punkweb_bb/widgets.py
+++ punkweb_bb/widgets.py
@@ -10,15 +10,14 @@ class BBCodeEditorWidget(forms.Textarea):
         css = {
             "all": (
                 "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/themes/square.min.css",
-                "/static/punkweb_bb/editor/bbcode-editor.css",
             )
         }
         js = (
             "/static/punkweb_bb/vendor/jquery-3.7.0.min.js",
             "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/jquery.sceditor.bbcode.min.js",
             "/static/punkweb_bb/vendor/sceditor-3.2.0/minified/icons/material.js",
-            "/static/punkweb_bb/editor/bbcode-editor-tags.js",
-            "/static/punkweb_bb/editor/bbcode-editor.js",
+            "/static/punkweb_bb/js/bbcode-editor-tags.js",
+            "/static/punkweb_bb/js/bbcode-editor.js",
         )
 
 
@@ -38,5 +37,5 @@ class MarkdownEditorWidget(forms.Textarea):
         js = (
             "/static/punkweb_bb/vendor/jquery-3.7.0.min.js",
             "/static/punkweb_bb/vendor/tiny-markdown-editor/dist/tiny-mde.min.js",
-            "/static/punkweb_bb/editor/markdown-editor.js",
+            "/static/punkweb_bb/js/markdown-editor.js",
         )
